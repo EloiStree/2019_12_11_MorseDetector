2be1f61e8a417d88f860977bc71d1e0cbe1a8dd5
streeeloi <streeeloi@gmail.com>
2019-12-11 16:50:03 +0100

    First commit

---
+++ Script/Detector/Editor/MorseDetectedEditor.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEditor;
+
+[CustomEditor(typeof(MorseDetector),true)]
+public class MorseDetectedEditor : Editor
+{
+    public string m_warningMessage="";
+    public override void OnInspectorGUI()
+    {
+        MorseDetector myTarget = (MorseDetector) target;
+        if (myTarget.m_morseSource == null)
+        {
+            m_warningMessage = "Please provide a morse emittor that use MorseEmittorInterface";
+        }
+        
+
+        if(m_warningMessage.Length>0)
+            EditorGUILayout.HelpBox(m_warningMessage, MessageType.Warning);
+
+        DrawDefaultInspector();
+        m_warningMessage = "";
+    }
+}
